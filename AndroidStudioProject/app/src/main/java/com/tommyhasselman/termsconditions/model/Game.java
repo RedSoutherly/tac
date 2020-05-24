2a4888f88e8246dbf7022031d3d9878046c8fa9c
David Epps <david@epps.co.nz>
2020-05-24 22:13:20 +1200

    Fleshed out basics of Game master class

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/Game.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/Game.java
@@ -1,4 +1,49 @@
 package com.tommyhasselman.termsconditions.model;
 
+import java.util.ArrayList;
+import java.util.Timer;
+
 public class Game {
+
+    //master list of objects (is this necessary?)
+    private ArrayList<GameObject> objects;
+    private Player player;
+    private enum Phase{
+            MAINMENU,
+            GAME,
+            CINIMATIC
+    };
+    private Timer time;
+
+    public Game (){
+        this.objects = new ArrayList<GameObject>();
+        time = new Timer();
+        //game by default in the alpha should be main menu
+        Phase phase = Phase.GAME;
+    }
+
+    /**
+     *
+     * starts the game
+     * in alpha it starts a timer and generates boxes
+     */
+    public void startGame(){
+        
+    }
+
+    public ArrayList<GameObject> getObjects() {
+        return objects;
+    }
+
+    public void setObjects(ArrayList<GameObject> objects) {
+        this.objects = objects;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
 }
