dab59a8303959d86a2e1e17c5328611362993e1f
Kezia Somerville-Petch <somervillepetch.kezia@gmail.com>
2020-05-24 21:19:36 +1200

    Created two lists in Box class, representing what is in the box and what should be in the box

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/Box.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/Box.java
@@ -1,4 +1,13 @@
 package com.tommyhasselman.termsconditions.model;
 
+import java.util.List;
+import java.util.ArrayList;
+
+/** Class Box that compares the contents of the box/parcel to what should be in the box/parcel */
 public class Box extends GameObject {
+    
+    List<OrderItem> box_contains = new ArrayList<OrderItem>();
+
+    List<OrderItem> box_should_contain = new ArrayList<OrderItem>();
+
 }
--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/DialougeBox.java
+++
@@ -1,4 +0,0 @@
-package com.tommyhasselman.termsconditions.model;
-
-public class DialougeBox extends GameObject {
-}
