44ffb3c38480eee2972bef9ba4320f9249ec7ba5
Tommy Hasselman <tommyhasselman@gmail.com>
2020-05-24 21:34:27 +1200

    Can generate test Items

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/MainActivity.java
@@ -3,12 +3,36 @@ package com.tommyhasselman.termsconditions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tommyhasselman.termsconditions.model.BasicItem;
+import com.tommyhasselman.termsconditions.model.OrderItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView testTextView;
+    Button testButton;
+    OrderItem item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testTextView = (TextView) findViewById(R.id.testTextView);
+        testButton = (Button) findViewById(R.id.testButton);
+        item = new BasicItem();
+        testTextView.setText(item.toString());
+
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                item = new BasicItem();
+                testTextView.setText(item.toString());
+            }
+        });
     }
+
 }
--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/OrderItem.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/OrderItem.java
@@ -1,4 +1,23 @@
 package com.tommyhasselman.termsconditions.model;
 
 public abstract class OrderItem extends GameObject {
+
+    String[] basicProducts = {
+            "Hat",
+            "Dildo",
+            "Ball"
+    };
+
+    String[] sizes = {
+            "Small",
+            "Medium",
+            "Large"
+    };
+
+    String[] colours = {
+            "Red",
+            "Green",
+            "Blue"
+    };
+
 }
