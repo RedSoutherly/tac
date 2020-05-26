95e7b58dd44eacf0a6ee5f428f798e2f95bd8207
Tommy Hasselman <tommyhasselman@gmail.com>
2020-05-26 19:30:23 +1200

    Added a product code identifyer to BasicItem, also commented

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/BasicItem.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/BasicItem.java
@@ -1,26 +1,58 @@
 package com.tommyhasselman.termsconditions.model;
 
+import androidx.annotation.NonNull;
+
 import java.util.Random;
 
 /**
+ * The Basic Item class is the first level of difficulty
+ * for matching product collections visually.
  *
+ * There are three aspects to each product of this type:
+ *  Size, Colour, and Product.
+ *
+ * The possible values for these fields are defined in the super class.
+ *
+ * @author Tommy Hasselman
  */
 public class BasicItem extends OrderItem {
 
-    String size;
-    String colour;
-    String product;
+    private String code;
+    private String size;
+    private String colour;
+    private String product;
 
+    /**
+     * Default constructor for Basic Item.
+     * Constructs an instance with all fields randomised.
+     */
     public BasicItem() {
         Random r = new Random();
-        size = sizes[r.nextInt(sizes.length)];
-        colour = colours[r.nextInt(colours.length)];
-        product = basicProducts[r.nextInt(basicProducts.length)];
+        int rs = r.nextInt(sizes.length);
+        int rc = r.nextInt(colours.length);
+        int rp = r.nextInt(basicProducts.length);
+        code = "SCP" + rs + rc + rp;
+        System.out.println(code);
+        size = sizes[rs];
+        colour = colours[rc];
+        product = basicProducts[rp];
     }
 
+    /**
+     * Custom toString() writes a description of the item.
+     * @return Returns the description as a String
+     */
+    @NonNull
     public String toString() {
         return (size+" "+colour+" "+product);
     }
 
+    /**
+     * Method for accessing the code field.
+     * @return Returns code String
+     */
+    public String getCode() {
+        return code;
+    }
 
 }
