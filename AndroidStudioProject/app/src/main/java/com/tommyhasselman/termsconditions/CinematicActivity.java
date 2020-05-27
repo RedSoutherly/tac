eb9135ef2a0a4d7b9814d5ff36cd8029a6161ee6
David Epps <david@epps.co.nz>
2020-05-27 15:25:39 +1200

    Cinematic added started writing scenarios

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/CinematicActivity.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/CinematicActivity.java
@@ -1,7 +1,17 @@
 package com.tommyhasselman.termsconditions;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.tommyhasselman.termsconditions.model.Cinematic;
+
 public class CinematicActivity extends AppCompatActivity {
-    
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Cinematic c = new Cinematic();
+
+
+    }
 }
