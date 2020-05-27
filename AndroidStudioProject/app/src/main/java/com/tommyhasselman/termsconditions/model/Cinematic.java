7d0f31ffbd3ba15f6812e652afb5598abc7ea075
Kezia Somerville-Petch <somervillepetch.kezia@gmail.com>
2020-05-27 16:09:54 +1200

    Created 4 scenarios, with two choices for each scenario, and created objects for them

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/Cinematic.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/Cinematic.java
@@ -1,15 +1,28 @@
 package com.tommyhasselman.termsconditions.model;
 
+import java.util.Random;
+
 public class Cinematic {
     public class Scenario{
-        private String[] Choice1{"pay for medication -50$"};
-        private String[] Choice2{"pray the sickness away"};
-        private String[] scenario{"your nephew is sick do you?"};
+        private String[] Choice1 = {"Pay for medication -$50", "Pay for home heating -$30", "Pay to fix the toilet -$70", "Book a psychologist appointment -$80"};
+        private String[] Choice2 = {"Pray the sickness away", "Endure the cold", "Attempt to fix it yourself", "Grin and bear it"};
+        private String[] Scenario = {"Your nephew is sick, do you;", "Winter is coming:", "Your toilet is broken, do you;", "Your mental health is at an all time low, do you;"};
+        public String firstChoice;
+        public String secondChoice;
+        public String scenarioChoice;
 
+        /** The constructor randomly chooses a scenario and assigns this scenario and
+         *  the associated choices to scenario and choice variable */
         public Scenario(){
+            Random rand = new Random();
+            int randomChoice = rand.nextInt(Scenario.length-1);
+            this.scenarioChoice = Scenario[randomChoice];
+            this.firstChoice = Choice1[randomChoice];
+            this.secondChoice = Choice2[randomChoice];
 
         }
     }
+
     public Cinematic(){
 
     }
