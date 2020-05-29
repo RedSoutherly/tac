6bf815a04b1abcd445a6378c27f4a4da43f079d3
David Epps <david@epps.co.nz>
2020-05-29 15:15:56 +1200

    Changed time implementation and pseudocoded it

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/MainActivity.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/MainActivity.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.tommyhasselman.termsconditions.model.Player;
+
+import java.util.Date;
 import java.util.TimerTask;
 import com.tommyhasselman.termsconditions.model.BasicItem;
 import com.tommyhasselman.termsconditions.model.Box;
 import com.tommyhasselman.termsconditions.model.OrderItem;
 
 import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         boxTextView = (TextView) findViewById(R.id.boxContents);
         generateButton = (Button) findViewById(R.id.generateButton);
         valid = (Button) findViewById(R.id.Valid);
-        invalid= (Button) findViewById(R.id.Invalid);
+        invalid = (Button) findViewById(R.id.Invalid);
 
         /**
          * When generateButton is clicked, generateNewBox() is called.
@@ -53,10 +58,10 @@ public class MainActivity extends AppCompatActivity {
                 invalid.setEnabled(true);
             }
         });
-        valid.setOnClickListener(new View.OnClickListener(){
+        valid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!b.isDiff()) {
+                if (!b.isDiff()) {
                     p.incrementScore();
                     valid.setBackgroundColor(0xFF0000);
                 }
@@ -71,10 +76,10 @@ public class MainActivity extends AppCompatActivity {
                 invalid.setEnabled(false);
             }
         });
-        invalid.setOnClickListener(new View.OnClickListener(){
+        invalid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(b.isDiff()) {
+                if (b.isDiff()) {
                     p.incrementScore();
                     invalid.setBackgroundColor(0xFF0000);
                 }
@@ -83,14 +88,21 @@ public class MainActivity extends AppCompatActivity {
                 invalid.setEnabled(false);
             }
         });
-        // this implementation is proboly bad maybe use timer and schedule task
         while(p.isAlive()){
-            long time= System.currentTimeMillis();
-            long end = time+15000;
-            if(time<end){
-                //set buttons to valid
+        TimerTask task = new TimerTask() {
+        
+
+            @Override
+            public void run() {
+                //display a countdown
+                //make buttons , score  usable and visable
             }
-            //set buttons to invalid and do a cinematic
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(task, new Date(), 3000);
+        //hide count down,score and buttons
+        //make a cinematic anc play it  then loop
         }
     }
 
