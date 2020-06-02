470525c9329205d489fb357ee4496ff74229d59b
David Epps <david@epps.co.nz>
2020-06-02 20:01:32 +1200

    small refactor and added a Cinematic activity

---
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/CinematicActivity.java
@@ -0,0 +1,18 @@
+package com.tommyhasselman.termsconditions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tommyhasselman.termsconditions.model.Cinematic;
+
+public class CinematicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cinematic);
+        Cinematic c = new Cinematic();
+        c.Scenario s = new c.Scenario();
+    }
+}
