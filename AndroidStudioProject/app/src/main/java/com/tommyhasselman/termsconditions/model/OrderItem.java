eb8a495e486b0dfceddd387d65f4fc30d87d11e6
Tommy Hasselman <tommyhasselman@gmail.com>
2020-08-16 18:37:17 +1200

    OrderItem JavaDoc

--- AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/OrderItem.java
+++ AndroidStudioProject/app/src/main/java/com/tommyhasselman/termsconditions/model/OrderItem.java
@@ -5,12 +5,19 @@ import android.graphics.Color;
 import com.tommyhasselman.termsconditions.R;
 
 /**
- * A very basic constructor for alpha that generates items with different random attributes.
+ * Order Item is the abstract class that defines values for use in the Item class.
  */
 public abstract class OrderItem {
 
+    /**
+     * The index boundary where the flavoured products begin.
+     */
     protected int plainProductBoundary = 5;
 
+    /**
+     * The String array of product types. All plain types are at the beginning of the array,
+     * followed by flavoured types.
+     */
     protected String[] productTypes = {
             "Ball",
             "Dildo",
@@ -28,10 +35,12 @@ public abstract class OrderItem {
             "Patterned Riot Shield",
 
             "BLM Shirt",
-            "Loli Shirt",
-            "MAGA Shirt"
+            "Loli Shirt"
     };
 
+    /**
+     * The int array of drawable resource values, which line up with the corresponding product types.
+     */
     protected int[] drawables = {
             R.drawable.p_ball_plain,
             R.drawable.p_dildo,
@@ -49,10 +58,13 @@ public abstract class OrderItem {
             R.drawable.p_riotshield_cute,
 
             R.drawable.p_shirt_blm,
-            R.drawable.p_shirt_loli,
-            R.drawable.p_shirt_maga
+            R.drawable.p_shirt_loli
     };
 
+    /**
+     * The int array of drawable resource values, which line up with the corresponding product types.
+     * These drawables have labels.
+     */
     protected int[] labbelledDrawables = {
             R.drawable.p_ball_plain_labelled,
             R.drawable.p_dildo_labelled,
@@ -70,22 +82,30 @@ public abstract class OrderItem {
             R.drawable.p_riotshield_cute_labelled,
 
             R.drawable.p_shirt_blm_labelled,
-            R.drawable.p_shirt_loli_labelled,
-            R.drawable.p_shirt_maga_labelled
+            R.drawable.p_shirt_loli_labelled
     };
 
+    /**
+     * String array of size values.
+     */
     protected String[] sizes = {
             "Small",
             "Medium",
             "Large"
     };
 
+    /**
+     * int array of size values.
+     */
     protected int[] intSizes = {
             250,
             350,
             450
     };
 
+    /**
+     * String array of colours.
+     */
     protected String[] colours = {
             "Red",
             "Green",
@@ -94,6 +114,9 @@ public abstract class OrderItem {
             "Magenta"
     };
 
+    /**
+     * int array of aRGB values.
+     */
     protected int[] argbColours = {
             Color.argb(50,255,0,0),
             Color.argb(50,0,255,0),
@@ -103,14 +126,23 @@ public abstract class OrderItem {
     };
 
     /**
-     * @return the unique identifying code for a given object
+     * @return Returns the unique identifying code for this item.
      */
     public abstract String getCode();
 
+    /**
+     * @return Returns the aRGB int value for this item.
+     */
     public abstract int getArgbColour();
 
+    /**
+     * @return Returns the int size value for this item.
+     */
     public abstract int getIntSize();
 
+    /**
+     * @return Returns the resource int value for the drawable of this item.
+     */
     public abstract int getDrawable();
 
 }
